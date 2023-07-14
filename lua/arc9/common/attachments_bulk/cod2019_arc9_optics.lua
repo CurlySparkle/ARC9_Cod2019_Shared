660e8060e3759956447a7eb90fd936f992eca2d7
May Lian <twilight_sparkle13@hotmail.com>
2023-07-14 10:35:08 -0400

    stuff

--- lua/arc9/common/attachments_bulk/cod2019_arc9_optics.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_optics.lua
@@ -455,7 +455,7 @@ ATT.Sights = {
 ATT.HoloSight = true
 ATT.HoloSightReticle = Material("hud/arc9_cod2019/reticles/aimpoint_dot.png", "mips smooth")
 ATT.HoloSightReticle:SetInt("$additive", 1)
-ATT.HoloSightSize = 1512
+ATT.HoloSightSize = 2048
 ATT.HoloSightColorable = false
 ATT.HoloSightColor = Color(255, 255, 255)
 
---
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_mike14.lua
@@ -0,0 +1,52 @@
+local ATT = {}
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.PrintName = "20-Rounds"
+ATT.CompactName = "20-R"
+ATT.Description = [[]]
+ATT.SortOrder = 1
+
+ATT.Icon = Material("entities/attachs/cod2019_mike14_xmags.png", "mips smooth")
+ATT.AutoStats = true
+
+ATT.Free = false
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/m14/attachment_vm_sn_mike14_xmags.mdl"
+ATT.BoneMerge = true
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Category = {"cod2019_mag_m14"}
+ATT.ActivateElements = {"mag_none","mag_xmag"}
+
+ATT.ClipSizeOverride = 20
+
+ATT.DropMagazineModel = "models/weapons/cod2019/attachs/weapons/m14/attachment_vm_sn_mike14_xmags.mdl"
+
+ARC9.LoadAttachment(ATT, "cod2019_m14_mag_20")
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.PrintName = "30-Rounds"
+ATT.CompactName = "30-R"
+ATT.Description = [[]]
+ATT.SortOrder = 2
+
+ATT.Icon = Material("entities/attachs/cod2019_mike14_xmags2.png", "mips smooth")
+ATT.AutoStats = true
+
+ATT.Free = false
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/m14/attachment_vm_sn_mike14_xmags2.mdl"
+ATT.BoneMerge = true
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Category = {"cod2019_mag_m14"}
+ATT.ActivateElements = {"mag_none","mag_xmag2"}
+
+ATT.ClipSizeOverride = 30
+
+ATT.DropMagazineModel = "models/weapons/cod2019/attachs/weapons/m14/attachment_vm_sn_mike14_xmags2.mdl"
+
+ARC9.LoadAttachment(ATT, "cod2019_m14_mag_30")
+---------------------------------------------------------------------------------------
