de59dff797a3e6449d2b8ab3621f619a44e07806
May Lian <twilight_sparkle13@hotmail.com>
2023-07-23 16:32:02 -0400

    go wacky

--- lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_akilo47.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_akilo47.lua
@@ -63,6 +63,77 @@ ARC9.LoadAttachment(ATT, "cod2019_akilo47_mag_smg")
 ATT = {}
 
 ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = "12-Gauge Mag"
+ATT.CompactName = "12-gauge"
+ATT.Description = [[Conversion kit to use mid caliber 12-gauge ammunition. Shorter range, faster fire rate, and less recoil to dominate close quarters engagements.]]
+
+ATT.Icon = Material("entities/attachs/cod2019_origin12_xmags.png", "mips smooth")
+
+--ATT.ShootSound = "Cod2019.ak47.smgcal.fire"
+--ATT.ShootSoundIndoor = "Cod2019.ak47.smgcal.fire"
+
+-- Non-Silenced Outside
+ATT.LayerSound = "Layer_Shotgun.Outside"
+ATT.DistantShootSound = "Distant_Sniper.Outside"
+-- Inside
+ATT.LayerSoundIndoor = "Layer_Sniper.Inside"
+ATT.DistantShootSoundIndoor = "Distant_Shotgun.Inside"
+---------------------------------------------------
+-- Silenced Outside
+ATT.LayerSoundSilenced = "Layer_ARSUP.Outside"
+ATT.DistantShootSoundSilenced = "Distant_Shotgun2.Outside"
+-- Inside
+ATT.LayerSoundSilencedIndoor = "Layer_ShotgunSUP.Inside"
+ATT.SoundSilencedIndoor = "Distant_Shotgun2.Outside"
+---------------------------------------------------
+
+ATT.ShellScale = 0.07
+
+ATT.ClipSizeOverride = 12
+ATT.RPM = 300
+
+ATT.RecoilMult = 2
+ATT.RecoilkickMult = 2
+
+ATT.DamageMaxMult = 0.9
+ATT.DamageMinMult = 0.9
+
+ATT.RangeMaxMult = 0.8
+
+ATT.DamageMax = 90 -- Damage done at point blank range
+ATT.DamageMin = 35 -- Damage done at maximum range
+
+ATT.DistributeDamage = true
+
+ATT.Spread = 175 * ARC9.MOAToAcc
+ATT.UseDispersion = true
+ATT.DispersionSpread = 0.01
+ATT.DispersionSpreadAddHipFire = 0.02
+ATT.SpreadMultHipFire = 0.7
+ATT.SpreadMultMove = 0.8
+
+ATT.VisualRecoilPunchMult = 2
+ATT.VisualRecoilUp = 2
+
+ATT.Num = 6
+
+ATT.SortOrder = 0
+ATT.Category = "cod2019_akilo47_mag"
+ATT.ActivateElements = {"mag_none"}
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/ak47/attachment_vm_ar_akilo47_originmag.mdl"
+ATT.DropMagazineModel = "models/weapons/cod2019/mags/w_shot_origin12_mag.mdl"
+ATT.BoneMerge = true
+
+ATT.ShellModel = "models/weapons/cod2019/shared/shell_origin12.mdl"
+ATT.ShellSounds = ARC9.ShotgunShellSoundsTable
+ATT.ShellScale = 1.2
+
+ARC9.LoadAttachment(ATT, "cod2019_akilo47_mag_origin12")
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
 ATT.PrintName = "Spetsnaz Elite"
 ATT.CompactName = "Spetsnaz Elite"
 ATT.Description = [[Heavy weight barrel used by special forces has increased muzzle velocity and improved range with little additional weight.]]
