c9d9f4de6b547ff0689c475c81524ce7cd089bca
May Lian <twilight_sparkle13@hotmail.com>
2023-08-11 01:27:22 -0400

    stuff

--- lua/arc9/common/attachments_bulk/cod2019_arc9_scope_m32.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_scope_m32.lua
@@ -1,7 +1,7 @@
 local ATT = {}
 
-ATT.PrintName = "Default Scope"
-ATT.CompactName = [[Default]]
+ATT.PrintName = "M32 Scope"
+ATT.CompactName = [[M32 Scope]]
 --ATT.Icon = Material("entities/attachs/go_optic_schmidt.png", "mips smooth")
 ATT.Description = [[Default scope for the MGL-32 Grenade Launcher.]]
 ATT.Pros = {
@@ -9,7 +9,7 @@ ATT.Pros = {
     "+ Clearer sight picture"
 }
 ATT.Cons = {}
-ATT.SortOrder = 4
+ATT.SortOrder = 100
 ATT.Free = false
 ATT.Folder = "Scopes"
 
@@ -36,8 +36,7 @@ ATT.RTScope = true
 ATT.RTScopeSubmatIndex = 2
 ATT.RTScopeFOV = 16
 ATT.RTScopeRes = 512
--- ATT.RTScopeSurface = Material("models/weapons/arc9/mw3/mw3_optics/hamr_lens")
-ATT.RTScopeReticle = Material("hud/arc9_cod2019/overlays/acog_crosshair.png", "mips smooth")
+ATT.RTScopeReticle = Material("hud/arc9_cod2019/overlays/m32_crosshair.png", "mips smooth")
 ATT.RTScopeReticleScale = 1
 ATT.RTScopeShadowIntensity = 1
 ATT.RTScopeNoPP = false
