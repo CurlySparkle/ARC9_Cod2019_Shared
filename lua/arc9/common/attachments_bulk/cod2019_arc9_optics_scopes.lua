41b1ab2289a7ef2c9b6b3a4db876019f0bf5268b
May Lian <twilight_sparkle13@hotmail.com>
2023-08-10 20:42:55 -0400

    added sniper scope

---
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_optics_scopes.lua
@@ -0,0 +1,62 @@
+local ATT = {}
+-------------------------------------------------------------------------
+ATT.PrintName = "Sniper Scope"
+ATT.CompactName = [[Sniper Scope]]
+ATT.Icon = Material("entities/attachs/cod2019_optic_snprscope_mike14.png", "mips smooth")
+ATT.Description = [[Long-range combat scope. Provides an improved precision at longer ranges.]]
+ATT.Pros = {
+    "+ 8x Zoom",
+    "+ Clearer sight picture"
+}
+ATT.Cons = {}
+ATT.SortOrder = 4
+ATT.Free = false
+ATT.Folder = "Scopes"
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Category = {"cod2019_optic"}
+ATT.ActivateElements = {"scope_sniper"}
+
+ATT.Model = "models/weapons/cod2019/attachs/sights/weapon_vm_scope_mike14.mdl"
+ATT.Scale = 1
+ATT.ModelOffset = Vector(-2, 0, 0.1)
+ATT.ModelAngleOffset = Angle(0, 0, 0)
+
+ATT.Sights = {
+    {
+        Pos = Vector(0, 7, -1.313),
+        Ang = Angle(0, 0, 0),
+        ViewModelFOV = 40,
+        Magnification = 2,
+        IgnoreExtra = true,
+		RTScopeFOV = 25,
+    },
+    {
+        Pos = Vector(0, 7, -1.313),
+        Ang = Angle(0, 0, 0),
+        ViewModelFOV = 40,
+        Magnification = 2,
+        IgnoreExtra = true,
+		RTScopeFOV = 15,
+    },
+    {
+        Pos = Vector(0, 7, -1.313),
+        Ang = Angle(0, 0, 0),
+        ViewModelFOV = 40,
+        Magnification = 2,
+        IgnoreExtra = true
+    },
+}
+
+ATT.RTScope = true
+ATT.RTScopeSubmatIndex = 1
+ATT.RTScopeFOV = 8
+ATT.RTScopeRes = 512
+ATT.RTScopeReticle = Material("hud/arc9_cod2019/overlays/mk4_crosshair.png", "mips smooth")
+ATT.RTScopeReticleScale = 1
+ATT.RTScopeShadowIntensity = 1
+ATT.RTScopeNoPP = false
+ATT.RTScopeColorable = false
+
+ARC9.LoadAttachment(ATT, "cod2019_optic_scope_mike14")
+-------------------------------------------------------------------------
