7bd8c12e263b6c0490683c6ad3505a771a1b5408
May Lian <twilight_sparkle13@hotmail.com>
2023-12-28 00:04:20 -0300

    vlk

--- lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_vlk.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_vlk.lua
@@ -23,6 +23,7 @@ ATT.ActivateElements = {"muzzle_none","barrel_none","barrel_vlk","vlk_barrel_hea
 --ATT.MuzzleParticleOverride = "weapon_muzzle_flash_assaultrifle_silenced2"
 --ATT.MuzzleParticleOverride_Priority = 10
 ATT.Override_MuzzleEffectAttachment = 1
+ATT.Override_MuzzleEffectAttachmentPriority = 3
 --ATT.MuzzleDevice = true
 
 ATT.AimDownSightsTimeMult = 1.25
@@ -52,3 +53,34 @@ end
 
 ARC9.LoadAttachment(ATT, "cod2019_vlk_barrel_heavy")
 ---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = "12-R Mag"
+ATT.CompactName = "12-R"
+ATT.Description = [[Extended magazines hold 12 rounds with a slight weight increase.]]
+
+ATT.Icon = Material("entities/attachs/cod2019_vlk_xmag.png", "mips smooth")
+
+ATT.ClipSizeOverride = 12
+
+ATT.SortOrder = 0
+ATT.Category = "cod2019_vlk_mag"
+ATT.ActivateElements = {"mag_none","mag_xmag"}
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/vlk/attachment_vm_sh_mike26_xmag.mdl"
+ATT.DropMagazineModel = "models/weapons/cod2019/attachs/weapons/vlk/attachment_vm_sh_mike26_xmag.mdl"
+ATT.BoneMerge = true
+
+ATT.BulletBones = {
+    [1] = "j_shell01",
+    [2] = "j_shell02",
+    [3] = "j_shell03",
+    [4] = "j_shell04",
+    [5] = "j_shell05",
+    [6] = "j_shell06",
+    [7] = "j_shell07",
+    [8] = "j_shell08",
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_vlk_mag_xmag")
