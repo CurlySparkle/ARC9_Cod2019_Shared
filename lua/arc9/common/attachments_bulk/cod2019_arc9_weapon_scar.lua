ecde03c3ce6b3ac8cd3c94aa7c8258521529a16d
May Lian <twilight_sparkle13@hotmail.com>
2023-12-31 14:39:07 -0300

    stuff

--- lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_scar.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_scar.lua
@@ -43,3 +43,40 @@ ATT.Element = {
 }
 
 ARC9.LoadAttachment(ATT, "cod2019_scar_barrel_long")
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.PrintName = "6.5mm Creedmoor Mag"
+ATT.CompactName = "6.5mm 10-Round"
+ATT.Description = [[]]
+ATT.SortOrder = 1
+
+ATT.Icon = Material("entities/attachs/cod2019_scar_mag_dmr.png", "mips smooth")
+ATT.AutoStats = true
+
+ATT.Free = false
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/scar/attachment_vm_ar_scharlie_mag_dmr.mdl"
+ATT.DropMagazineModel = "models/weapons/cod2019/attachs/weapons/scar/attachment_vm_ar_scharlie_mag_dmr.mdl"
+ATT.BoneMerge = true
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Category = {"cod2019_scar_mag"}
+ATT.ActivateElements = {"mag_none","mag_dmr"}
+
+ATT.ClipSizeOverride = 10
+
+ATT.RecoilMult = 1.15
+ATT.RecoilkickMult = 1.20
+
+ATT.DamageMaxMult = 1.55
+ATT.DamageMinMult = 1.55
+ATT.PhysBulletMuzzleVelocityMult = 1.15
+
+ATT.BulletBones = {
+    [1] = "j_bullet1",
+    [2] = "j_bullet2",
+	[3] = "j_bullet3",
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_scar_mag_dmr")
