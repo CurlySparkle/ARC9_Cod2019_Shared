ad4408a8978234db1686421b08ca0fc8e976f2f7
May Lian <twilight_sparkle13@hotmail.com>
2024-01-25 12:59:13 -0300

    x16

--- lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_x16.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_x16.lua
@@ -9,11 +9,13 @@ ATT.SortOrder = 1
 
 ATT.Icon = Material("entities/attachs/cod2019_pi_x16_slide_auto.png", "mips smooth")
 ATT.AutoStats = true
-
 ATT.Free = false
 
-ATT.Model = "models/weapons/cod2019/attachs/weapons/x16/attachment_vm_pi_golf21_akimbo_slide_auto.mdl"
+ATT.Model = "models/weapons/cod2019/attachs/weapons/x16/attachment_vm_pi_golf21_slide_auto_akimbo.mdl"
 ATT.BoneMerge = true
+
+ATT.MuzzleDevice = true -- set to true if you want to use this to emit particles
+ATT.MuzzleDevice_Priority = 3
 
 ATT.MenuCategory = "ARC9 - MW2019 Attachments"
 ATT.Category = {"cod2019_x16_akimbo_slide"}
@@ -26,36 +28,156 @@ ATT.FiremodesOverride = {
     },
 }
 
---ATT.Silencer = true
---ATT.MuzzleParticleOverride = "weapon_muzzle_flash_assaultrifle_silenced2"
---ATT.MuzzleParticleOverride_Priority = 10
---ATT.Override_MuzzleEffectAttachment = 1
---ATT.MuzzleDevice = true
-
---ATT.AimDownSightsTimeMult = 1.25
---ATT.SprintToFireTimeMult = 1.25
 ATT.RecoilMult = 1.1
-
 ATT.RangeMaxMult = 0.95
 ATT.RangeMinMult = 0.95
 ATT.PhysBulletMuzzleVelocityMult = 0.95
+ATT.AimDownSightsTimeMult = 1.15
 
--- ATT.Attachments = {
-    -- {
-        -- PrintName = ARC9:GetPhrase("mw19_category_muzzle2"),
-        -- Pos = Vector(-7.5, 0, 0),
-        -- Ang = Angle(0, 0, 0),
-        -- Icon_Offset = Vector(0, 0, 0),
-        -- Category = {"muzzle_shotgun","muzzle"},
-		-- Scale = 1
-    -- }
--- }
-
--- ATT.DrawFunc = function(swep, model, wm)
-    -- if swep:GetElements()["muzzle_none"] then
-        -- model:SetBodygroup(1,1)
-    -- end
--- end
-
-ARC9.LoadAttachment(ATT, "cod2019_x16_akimbo_slide_auto")
+ARC9.LoadAttachment(ATT, "cod2019_x16_slide_auto_akimbo")
 ---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.PrintName = "17 Round Mags"
+ATT.CompactName = "17-Round"
+ATT.Description = [[High capacity magazines hold 17 rounds with a moderate weight increase.]]
+ATT.SortOrder = 1
+
+ATT.Icon = Material("entities/attachs/cod2019_pi_x16_mag_xmags.png", "mips smooth")
+ATT.AutoStats = true
+ATT.Free = false
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/x16/attachment_vm_pi_golf21_mag_xmags_akimbo.mdl"
+ATT.DropMagazineModel = "models/weapons/cod2019/attachs/weapons/x16/attachment_vm_pi_golf21_mag_xmags.mdl"
+ATT.BoneMerge = true
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Category = {"cod2019_x16_akimbo_mag"}
+ATT.ActivateElements = {"mag_none","mag_xmag"}
+
+ATT.AimDownSightsTimeMult = 1.1
+ATT.ClipSizeOverride = 17
+ATT.ReloadTimeMult = 1.1
+
+ATT.BulletBones = {
+    [1] = "j_bullet",
+    [2] = "j_bullet2",
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_x16_akimbo_mag_xmag")
+----------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.PrintName = "X16 Stock"
+ATT.CompactName = "X16 Stock"
+ATT.Description = [[X16 Stock]]
+ATT.SortOrder = 1
+
+ATT.Icon = Material("entities/attachs/cod2019_pi_x16_slide_auto.png", "mips smooth")
+ATT.AutoStats = true
+ATT.Free = false
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/x16/attachment_vm_pi_golf21_stock_akimbo.mdl"
+ATT.BoneMerge = true
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Category = {"cod2019_x16_akimbo_stock"}
+ATT.ActivateElements = {"stock_none"}
+
+ATT.RecoilKickMult = 0.8
+ATT.RecoilkMult = 0.8
+ATT.DeployTimeMult = 1.15
+ATT.AimDownSightsTimeMult = 1.1
+
+ARC9.LoadAttachment(ATT, "cod2019_x16_stock_akimbo")
+--------------------------------------------------------------------------------------- SINGLE
+ATT = {}
+
+ATT.PrintName = "Singuard Arms Featherweight"
+ATT.CompactName = "Featherweight"
+ATT.Description = [[Ported slide and large bore barrel reduces weight for faster gun handling at a slight cost to range.]]
+ATT.SortOrder = 1
+
+ATT.Icon = Material("entities/attachs/cod2019_pi_x16_slide_auto.png", "mips smooth")
+ATT.AutoStats = true
+ATT.Free = false
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/x16/attachment_vm_pi_golf21_slide_auto.mdl"
+ATT.BoneMerge = true
+
+ATT.MuzzleDevice = true -- set to true if you want to use this to emit particles
+ATT.MuzzleDevice_Priority = 3
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Category = {"cod2019_x16_slide"}
+ATT.ActivateElements = {"slide_none"}
+
+ATT.FiremodesOverride = {
+    {
+        Mode = -1,
+		RPM = 1200,
+    },
+}
+
+ATT.RecoilMult = 1.1
+ATT.RangeMaxMult = 0.95
+ATT.RangeMinMult = 0.95
+ATT.PhysBulletMuzzleVelocityMult = 0.95
+ATT.AimDownSightsTimeMult = 1.15
+
+ARC9.LoadAttachment(ATT, "cod2019_x16_slide_auto")
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.PrintName = "17 Round Mags"
+ATT.CompactName = "17-Round"
+ATT.Description = [[High capacity magazines hold 17 rounds with a moderate weight increase.]]
+ATT.SortOrder = 1
+
+ATT.Icon = Material("entities/attachs/cod2019_pi_x16_mag_xmags.png", "mips smooth")
+ATT.AutoStats = true
+ATT.Free = false
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/x16/attachment_vm_pi_golf21_mag_xmags.mdl"
+ATT.DropMagazineModel = "models/weapons/cod2019/attachs/weapons/x16/attachment_vm_pi_golf21_mag_xmags.mdl"
+ATT.BoneMerge = true
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Category = {"cod2019_x16_mag"}
+ATT.ActivateElements = {"mag_none","mag_xmag"}
+
+ATT.AimDownSightsTimeMult = 1.1
+ATT.ClipSizeOverride = 17
+ATT.ReloadTimeMult = 1.1
+
+ATT.BulletBones = {
+    [1] = "j_bullet",
+    [2] = "j_bullet2",
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_x16_mag_xmag")
+----------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.PrintName = "X16 Stock"
+ATT.CompactName = "X16 Stock"
+ATT.Description = [[X16 Stock]]
+ATT.SortOrder = 1
+
+ATT.Icon = Material("entities/attachs/cod2019_pi_x16_slide_auto.png", "mips smooth")
+ATT.AutoStats = true
+ATT.Free = false
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/x16/attachment_vm_pi_golf21_stock.mdl"
+ATT.BoneMerge = true
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Category = {"cod2019_x16_stock"}
+ATT.ActivateElements = {"stock_none"}
+
+ATT.RecoilKickMult = 0.8
+ATT.RecoilkMult = 0.8
+ATT.DeployTimeMult = 1.15
+ATT.AimDownSightsTimeMult = 1.1
+
+ARC9.LoadAttachment(ATT, "cod2019_x16_stock")
