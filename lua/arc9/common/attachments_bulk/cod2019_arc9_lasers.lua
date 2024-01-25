f86fefa7cd7f117aba16da44e93f0acdbcda424a
MokaAkashiya85 <ThaERROR404@gmail.com>
2024-01-25 15:18:35 +0100

    Update cod2019_arc9_lasers.lua
    - Changed "PeekCrosshair" to "NoPeekCrosshair"

--- lua/arc9/common/attachments_bulk/cod2019_arc9_lasers.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_lasers.lua
@@ -3,20 +3,20 @@ local ATT = {}
 local stats_tac = {
     AimDownSightsTimeAdd = -0.035,
 	SpreadMult = 0.90,
-	PeekCrosshair = false,
+	NoPeekCrosshair = true,
 }
 
 local stats_1mw = {
     SpreadMultMove = 0.94,
 	SpreadMult = 0.80,
-	PeekCrosshair = false,
+	NoPeekCrosshair = true,
 }
 
 local stats_5mw = {
     SprintToFireTimeAdd = -0.05,
     SpreadMultMove = 0.94,
 	SpreadMult = 0.85,
-	PeekCrosshair = false,
+	NoPeekCrosshair = true,
 }
 
 -------------------------------------------------------------------------------
