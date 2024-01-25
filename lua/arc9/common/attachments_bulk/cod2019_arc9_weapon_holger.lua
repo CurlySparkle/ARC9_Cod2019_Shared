10ecddf38d209e95c62f3ddc7b17ddef58b94a67
May Lian <twilight_sparkle13@hotmail.com>
2024-01-25 01:48:55 -0300

    Update cod2019_arc9_weapon_holger.lua

--- lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_holger.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_holger.lua
@@ -17,15 +17,8 @@ ATT.DropMagazineModel = "models/weapons/cod2019/attachs/mag/holger_armag.mdl"
 ATT.BoneMerge = true
 
 ATT.ClipSizeOverride = 30
-
-ATT.DamageMaxMult = 0.85
-ATT.DamageMinMult = 0.85
-ATT.SpreadMult = 1.15
-ATT.RecoilMult = 0.85
-ATT.RecoilKickMult = 0.9
-
-ATT.AimDownSightsTimeMult = 0.75
-ATT.DeployTimeMult = 0.75
+ATT.AimDownSightsTimeMult = 0.85
+ATT.DeployTimeMult = 0.85
 
 ATT.DropMagazineSounds = {
 "weapons/cod2019/shared/magazine_drops/iw8_phys_mag_drop_ar_poly_concrete_01.ogg", 
@@ -66,32 +59,46 @@ ATT.BoneMerge = true
 
 ATT.MenuCategory = "ARC9 - MW2019 Attachments"
 ATT.Category = {"cod2019_holger_barrel"}
-ATT.ActivateElements = {"barrel_none","muzzle_none","barrel_spitfire"}
+ATT.ActivateElements = {"barrel_none","muzzle_none"}
 
-ATT.Attachments = {
-    {
-        PrintName = ARC9:GetPhrase("mw19_category_muzzle2"),
-        Pos = Vector(-10.5, 0, 0.35),
-        Ang = Angle(0, 0, 0),
-        Icon_Offset = Vector(0, 0, 0),
-        Category = "cod2019_muzzle",
-		Scale = 1,
-    },
-    {
-        PrintName = ARC9:GetPhrase("mw19_category_laser2"),
-        Pos = Vector(-7, 1.2, 0.3),
-        Ang = Angle(0, 0, -90),
-        Icon_Offset = Vector(0, 0, 0),
-        Category = "cod2019_tac",
-		Scale = 1
-    },
-    {
-        PrintName = ARC9:GetPhrase("mw19_category_underbarrel2"),
-        Pos = Vector(-6.5, 0, 1.77),
-        Ang = Angle(0, 0, 180),
-        Icon_Offset = Vector(0, 0, 0),
-        Category = "cod2019_grip",
-		Scale = 1
+ATT.AimDownSightsTimeMult = 0.95
+ATT.DeployTimeMult = 0.95
+ATT.RangeMaxMult = 0.95
+ATT.SpreadMult = 1.05
+
+-- ATT.Attachments = {
+    -- {
+        -- PrintName = ARC9:GetPhrase("mw19_category_muzzle2"),
+        -- Pos = Vector(-10.5, 0, 0.35),
+        -- Ang = Angle(0, 0, 0),
+        -- Icon_Offset = Vector(0, 0, 0),
+        -- Category = "cod2019_muzzle",
+		-- Scale = 1,
+    -- },
+    -- {
+        -- PrintName = ARC9:GetPhrase("mw19_category_laser2"),
+        -- Pos = Vector(-7, 1.2, 0.3),
+        -- Ang = Angle(0, 0, -90),
+        -- Icon_Offset = Vector(0, 0, 0),
+        -- Category = "cod2019_tac",
+		-- Scale = 1
+    -- },
+    -- {
+        -- PrintName = ARC9:GetPhrase("mw19_category_underbarrel2"),
+        -- Pos = Vector(-6.5, 0, 1.77),
+        -- Ang = Angle(0, 0, 180),
+        -- Icon_Offset = Vector(0, 0, 0),
+        -- Category = "cod2019_grip",
+		-- Scale = 1
+    -- }
+-- }
+
+ATT.Element = {
+    AttPosMods = {
+        [2] = { -- slot of the weapon's attachment
+            Pos = Vector(-4, 0, 0),
+            Ang = Angle(0,0,0),
+        }
     }
 }
 
