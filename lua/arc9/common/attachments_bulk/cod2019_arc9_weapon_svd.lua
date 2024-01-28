9c180e31b2be8ca70c6f1a3d556fd50125d3a4aa
May Lian <twilight_sparkle13@hotmail.com>
2024-01-28 02:38:11 -0300

    stuff

--- lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_svd.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_svd.lua
@@ -1,4 +1,129 @@
 local ATT = {}
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = "660mm Extended Barrel"
+ATT.CompactName = "660mm"
+ATT.Description = [[Extended barrel improves muzzle velocity and range. Additional weight stabilize shots but hinders mobility.]]
+
+ATT.Icon = Material("entities/attachs/cod2019_sn_svd_barlong.png", "mips smooth")
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/svd/attachment_vm_sn_delta_barlong.mdl"
+ATT.BoneMerge = false
+
+ATT.SortOrder = 0
+ATT.Category = "cod2019_svd_barrel"
+ATT.ActivateElements = {"barrel_none","muzzle_none"}
+
+ATT.MuzzleDevice = true -- set to true if you want to use this to emit particles
+ATT.MuzzleDevice_Priority = 2
+
+ATT.AimDownSightsTimeMult = 1.15
+ATT.RecoilMult = 0.9
+
+ATT.RangeMaxMult = 1.25
+ATT.RangeMinMult = 1.25
+
+ATT.DrawFunc = function(swep, model, wm)
+    if swep:GetElements()["muzzle"] then
+        model:SetBodygroup(1,1)
+    end
+end
+
+ATT.Element = {
+    AttPosMods = {
+        [3] = { -- slot of the weapon's attachment
+            Pos = Vector(3.1, 0, 0),
+            Ang = Angle(0,0,0),
+        }
+    }
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_svd_barrel_long")
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = "510mm Compact Barrel"
+ATT.CompactName = "510mm"
+ATT.Description = [[Short, compact barrel sacrifices accuracy and range for speed and agility. Better for sniping on the move.]]
+
+ATT.Icon = Material("entities/attachs/cod2019_sn_svd_barshort.png", "mips smooth")
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/svd/attachment_vm_sn_delta_barshort.mdl"
+ATT.BoneMerge = false
+
+ATT.SortOrder = 0
+ATT.Category = "cod2019_svd_barrel"
+ATT.ActivateElements = {"barrel_none","muzzle_none"}
+ATT.ModelOffset = Vector(0, 0, 0)
+
+ATT.MuzzleDevice = true -- set to true if you want to use this to emit particles
+ATT.MuzzleDevice_Priority = 2
+
+ATT.AimDownSightsTimeMult = 0.85
+ATT.RecoilMult = 1.15
+
+ATT.RangeMaxMult = 0.85
+ATT.RangeMinMult = 0.85
+
+ATT.DrawFunc = function(swep, model, wm)
+    if swep:GetElements()["muzzle"] then
+        model:SetBodygroup(1,1)
+    end
+end
+
+ATT.Element = {
+    AttPosMods = {
+        [3] = { -- slot of the weapon's attachment
+            Pos = Vector(-5, 0, 0),
+            Ang = Angle(0,0,0),
+        }
+    }
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_svd_barrel_short")
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = "15 Round Mag"
+ATT.CompactName = "15-Round"
+ATT.Description = [[Extended magazines hold 15 rounds of 7.62×54mmR with a slight weight increase.]]
+
+--ATT.Icon = Material("entities/attachs/go_aug_ammo_9mm.png", "mips smooth")
+
+ATT.ClipSizeOverride = 20
+ATT.ReloadTimeMult = 1.1
+ATT.AimDownSightsTimeMult = 1.1
+
+ATT.SortOrder = 0
+ATT.Category = "cod2019_svd_mag"
+ATT.ActivateElements = {"mag_none","mag_xmag"}
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/svd/attachment_vm_sn_delta_xmags.mdl"
+ATT.DropMagazineModel = "models/weapons/cod2019/attachs/weapons/svd/attachment_vm_sn_delta_xmags.mdl"
+ATT.BoneMerge = true
+
+ARC9.LoadAttachment(ATT, "cod2019_svd_mag_30")
+---------------------------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = "Sling"
+ATT.CompactName = "Sling"
+ATT.Description = [[]]
+
+--ATT.Icon = Material("entities/attachs/go_aug_ammo_9mm.png", "mips smooth")
+
+ATT.SortOrder = 0
+ATT.Category = "cod2019_svd_extras"
+
+ATT.Model = "models/weapons/cod2019/attachs/weapons/svd/attachment_vm_sn_delta_sling.mdl"
+ATT.BoneMerge = true
+
+ARC9.LoadAttachment(ATT, "cod2019_svd_sling")
 ----------------------------------------------------------------------------------------
 ATT = {}
 
@@ -83,26 +208,3 @@ ATT.Attachments = {
 }
 
 ARC9.LoadAttachment(ATT, "cod2019_svd_stock_v2")
----------------------------------------------------------------------------------------
-ATT = {}
-
-ATT.MenuCategory = "ARC9 - MW2019 Attachments"
-ATT.PrintName = "15 Round Mag"
-ATT.CompactName = "15-Round"
-ATT.Description = [[Extended magazines hold 15 rounds of 7.62×54mmR with a slight weight increase.]]
-
---ATT.Icon = Material("entities/attachs/go_aug_ammo_9mm.png", "mips smooth")
-
-ATT.ClipSizeOverride = 20
-ATT.ReloadTimeMult = 1.1
-ATT.AimDownSightsTimeMult = 1.1
-
-ATT.SortOrder = 0
-ATT.Category = "cod2019_svd_mag"
-ATT.ActivateElements = {"mag_none","mag_xmag"}
-
-ATT.Model = "models/weapons/cod2019/attachs/weapons/svd/attachment_vm_sn_delta_xmags.mdl"
-ATT.DropMagazineModel = "models/weapons/cod2019/attachs/weapons/svd/attachment_vm_sn_delta_xmags.mdl"
-ATT.BoneMerge = true
-
-ARC9.LoadAttachment(ATT, "cod2019_svd_mag_30")
