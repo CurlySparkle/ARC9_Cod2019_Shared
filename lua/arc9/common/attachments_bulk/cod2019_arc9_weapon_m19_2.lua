cdec8515447dde842621f2a2aa909f5fc895c509
MokaAkashiya85 <ThaERROR404@gmail.com>
2024-02-04 14:48:16 +0100

    -Akimbo
    - Removed Akimbo-specific attachments for handguns; They now use standard attachments with model and stat overrides.

--- lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_m19_2.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_weapon_m19_2.lua
@@ -1,5 +1,5 @@
 local ATT = {}
----------------------------------------------------------------------------------------- SINGLE
+----------------------------------------------------------------------------------------
 ATT = {}
 
 ATT.PrintName = "XRK V Extended"
@@ -39,43 +39,4 @@ ATT.Element = {
 }
 
 ARC9.LoadAttachment(ATT, "cod2019_m19_slide_ext")
---------------------------------------------------------------------------------------- AKIMBO
-ATT = {}
-
-ATT.PrintName = "XRK V Extended"
-ATT.CompactName = "XRK V Ext"
-ATT.Description = [[Extended slide slightly increases muzzle velocity and improves range. Lightweight construction adds only a minor increase to weight.]]
-ATT.SortOrder = 1
-
-ATT.Icon = Material("entities/attachs/cod2019_pi_50gs_slide_extended02.png", "mips smooth")
-ATT.AutoStats = true
-ATT.Free = false
-
-ATT.Model = "models/weapons/cod2019/attachs/weapons/m19/attachment_vm_pi_papa320_barrel_ext_akimbo.mdl"
-ATT.BoneMerge = true
-
-ATT.MenuCategory = "ARC9 - MW2019 Attachments"
-ATT.Category = {"cod2019_m19_slide_akimbo"}
-ATT.ActivateElements = {"slide_none","slide_extended"}
-
-ATT.MuzzleDevice_Priority = 1
-ATT.MuzzleDevice = true
-
-ATT.AimDownSightsTimeMult = 1.1
-ATT.SprintToFireTimeMult = 1.1
-
-ATT.RecoilMult = 0.95
-ATT.RangeMaxMult = 1.15
-ATT.RangeMinMult = 1.15
-ATT.PhysBulletMuzzleVelocityMult = 1.15
-
-ATT.Element = {
-    AttPosMods = {
-        [2] = { -- slot of the weapon's attachment
-            Pos = Vector(0.41, 0, 0),
-            Ang = Angle(0,0,0),
-        }
-    }
-}
-
-ARC9.LoadAttachment(ATT, "cod2019_m19_slide_ext_akimbo")
+----------------------------------------------------------------------------------------
