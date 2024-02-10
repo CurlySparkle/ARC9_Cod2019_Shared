ad817d2c49ee3b49e736883b2101ebebea9bcdc5
May Lian <twilight_sparkle13@hotmail.com>
2024-02-09 22:34:20 -0300

    stuff

--- lua/arc9/common/attachments_bulk/cod2019_arc9_stocks.lua
+++ lua/arc9/common/attachments_bulk/cod2019_arc9_stocks.lua
@@ -111,6 +111,41 @@ ARC9.LoadAttachment(ATT, "cod2019_stock_tube_medium")
 -------------------------------------------------------------------------------
 ATT = {}
 
+ATT.PrintName = [[Singuard Buffer Tube]]
+ATT.CompactName = [[Singuard Tube]]
+ATT.Icon = Material("entities/attachs/cod2019_stock_tube_singuard.png")
+ATT.Description = [[Aluminium mounting point tube, improves sight speed with the cost of recoil.]]
+ATT.SortOrder = 0
+ATT.Free = false
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Category = {"cod2019_tube"}
+ATT.Model = "models/weapons/cod2019/attachs/stocks/attachment_vm_tube_singuard.mdl"
+--ATT.Scale = 1
+ATT.ModelOffset = Vector(0, 0, 0)
+-- ATT.ModelAngleOffset = Angle(0,-90,0)
+ATT.Folder = ARC9:GetPhrase("mw19_folder_tube")
+ATT.IconOffset = Vector(0, 0, 0)
+ATT.ActivateElements = {"stock_adapter"}
+
+ATT.AimDownSightsTimeMult = 0.92
+ATT.RecoilMult = 1.07
+
+ATT.Attachments = {
+    {
+        PrintName = ARC9:GetPhrase("mw19_category_stock2"),
+        Pos = Vector(-1.3, 0, 0),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(-3, 0, 0),
+        Category = {"cod2019_stocks"},
+		Scale = 1
+    }
+}
+
+ARC9.LoadAttachment(ATT, "cod2019_stock_tube_singuard")
+-------------------------------------------------------------------------------
+ATT = {}
+
 ATT.MenuCategory = "ARC9 - MW2019 Attachments"
 ATT.PrintName = "FORGE TAC Stalker"
 ATT.CompactName = [[Stalker]]
@@ -121,10 +156,10 @@ ATT.SortOrder = 5
 ATT.AutoStats = true
 ATT.Category = "cod2019_stocks"
 
-ATT.Model = "models/weapons/cod2019/attachs/stocks/stock_heavy.mdl"
+ATT.Model = "models/weapons/cod2019/attachs/stocks/attachment_vm_stock_heavy.mdl"
 
 ATT.Scale = 1
-ATT.ModelOffset = Vector(0, 0, -0.03)
+ATT.ModelOffset = Vector(-1.3, 0, -0.02)
 
 table.Merge(ATT, stats_heavy)
 
@@ -142,10 +177,10 @@ ATT.SortOrder = 5.1
 ATT.AutoStats = true
 ATT.Category = "cod2019_stocks"
 
-ATT.Model = "models/weapons/cod2019/attachs/stocks/stock_light01.mdl"
+ATT.Model = "models/weapons/cod2019/attachs/stocks/attachment_vm_stock_light01.mdl"
 
 ATT.Scale = 1
-ATT.ModelOffset = Vector(0.15, 0, -0.03)
+ATT.ModelOffset = Vector(-1.3, 0, -0.02)
 
 table.Merge(ATT, stats_light)
 
@@ -163,10 +198,10 @@ ATT.SortOrder = 5.2
 ATT.AutoStats = true
 ATT.Category = "cod2019_stocks"
 
-ATT.Model = "models/weapons/cod2019/attachs/stocks/stock_medium01.mdl"
+ATT.Model = "models/weapons/cod2019/attachs/stocks/attachment_vm_stock_medium01.mdl"
 
 ATT.Scale = 1
-ATT.ModelOffset = Vector(0, 0, 0.05)
+ATT.ModelOffset = Vector(-1.3, 0, 0.03)
 
 table.Merge(ATT, stats_medium)
 
@@ -184,14 +219,35 @@ ATT.SortOrder = 5.3
 ATT.AutoStats = true
 ATT.Category = "cod2019_stocks"
 
-ATT.Model = "models/weapons/cod2019/attachs/stocks/stock_heavy02.mdl"
+ATT.Model = "models/weapons/cod2019/attachs/stocks/attachment_vm_stock_heavy02.mdl"
 
 ATT.Scale = 1
-ATT.ModelOffset = Vector(0, 0, -0.05)
+ATT.ModelOffset = Vector(-1.3, 0, 0)
 
 table.Merge(ATT, stats_heavy)
 
 ARC9.LoadAttachment(ATT, "cod2019_stock_heavy02")
+-------------------------------------------------------------------------------
+ATT = {}
+
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.PrintName = "Singuard Arms Invader"
+ATT.CompactName = [[Singuard]]
+ATT.Description = [[Aluminum core synthetic stock designed for agility while aiming down sights.]]
+ATT.Icon = Material("entities/attachs/cod2019_stocks_mike4_stock_v2.png", "mips smooth")
+ATT.SortOrder = 5.4
+
+ATT.AutoStats = true
+ATT.Category = "cod2019_stocks"
+
+ATT.Model = "models/weapons/cod2019/attachs/stocks/attachment_vm_ar_mike4_stock_v2.mdl"
+
+ATT.Scale = 1
+ATT.ModelOffset = Vector(-1.3, 0, 0)
+
+ATT.AimDownSightsTimeAdd = 0.15
+
+ARC9.LoadAttachment(ATT, "cod2019_stock_singuard")
 -------------------------------------------------------------------------------
 ATT = {}
 
@@ -203,9 +259,10 @@ ATT.Icon = Material("entities/attachs/cod2019_stock_non.png", "mips smooth")
 ATT.SortOrder = 5.4
 
 ATT.AutoStats = true
-ATT.Category = "cod2019_stocks_none"
+ATT.Category = "cod2019_tube"
 
-ATT.Model = "models/weapons/cod2019/attachs/stocks/stock_none.mdl"
+ATT.ActivateElements = {"stock_adapter"}
+ATT.Model = "models/weapons/cod2019/attachs/stocks/attachment_vm_stock_no.mdl"
 
 ATT.Scale = 1
 ATT.ModelOffset = Vector(0, 0, 0)
