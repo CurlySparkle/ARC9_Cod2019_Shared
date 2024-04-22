6ed8f2eb30b48d6adfec185893ede19bfa517140
May Lian <twilight_sparkle13@hotmail.com>
2024-04-21 23:05:20 -0400

    Update mw19_shared_killcounter.lua

--- lua/arc9/common/attachments_bulk/mw19_shared_killcounter.lua
+++ lua/arc9/common/attachments_bulk/mw19_shared_killcounter.lua
@@ -1,17 +1,18 @@
 local ATT = {}
-
+------------------------------------------------------------------------
 ATT = {}
 
-ATT.PrintName = "Kill Counter"
-ATT.CompactName = "KILLS"
+ATT.PrintName = "Gun Screen (Kill Counter)"
+ATT.CompactName = "Gun Screen K"
 ATT.Icon = Material("entities/attachs/cod2019_attach_cosmetic_killcounter.png", "mips smooth")
 ATT.Free = true
 
 ATT.Description = [[Device for tracking your weapon's kill stats.]]
-ATT.MenuCategory = "ARC9 - Charms"
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Folder = "MW19"
 
 ATT.Model = "models/weapons/cod2019/attachs/cosmetic/cos_vm_iw9_mounteddisplay01_v0.mdl"
-ATT.ModelOffset = Vector(0.1, 0, 0)
+ATT.ModelOffset = Vector(0, 0.1, 0)
 ATT.BoneMerge = false
 ATT.Category = {"charm", "gunscreen"}
 
@@ -116,3 +117,69 @@ if CLIENT then
 end
 
 ARC9.LoadAttachment(ATT, "cod2019_cosmetic_killcounter")
+------------------------------------------------------------------------
+ATT = {}
+
+ATT.PrintName = "Gun Screen (Time Clock)"
+ATT.CompactName = "Gun Screen C"
+ATT.Icon = Material("entities/attachs/cod2019_attach_cosmetic_killcounter.png", "mips smooth")
+ATT.Free = true
+
+ATT.Description = [[Device for tracking your real time hour.]]
+ATT.MenuCategory = "ARC9 - MW2019 Attachments"
+ATT.Folder = "MW19"
+
+ATT.Model = "models/weapons/cod2019/attachs/cosmetic/cos_vm_iw9_mounteddisplay01_v0.mdl"
+ATT.ModelOffset = Vector(0, 0.1, 0)
+ATT.BoneMerge = false
+ATT.Category = {"charm", "gunscreen"}
+
+if CLIENT then
+    local rtmat = GetRenderTarget("arc9_gunscreen", 256, 256, false)
+
+    ATT.DrawFunc = function(self, model, wm)
+        if wm then return end
+
+        render.PushRenderTarget(rtmat, 0, 0, 256, 256)
+
+        render.Clear(0, 0, 0, 0)
+
+        cam.Start2D()
+
+        local text = os.date("%H:%M")
+
+        if CurTime() % 2 < 1 then
+            text = string.Replace(text, ":", " ")
+        end
+
+        surface.SetFont("ARC9_48_LCD")
+
+        local w, h = surface.GetTextSize(text)
+
+        surface.SetTextPos(128 - w / 2, 128 - h / 2 - 4)
+        surface.SetTextColor(255, 255, 255, 255)
+        surface.DrawText(text)
+
+        local text_date = os.date("%d %b")
+
+        surface.SetFont("ARC9_32_LCD")
+
+        local w_date, h_date = surface.GetTextSize(text_date)
+
+        surface.SetTextPos(128 - w_date / 2, 128 - h_date / 2 + 45)
+        surface.SetTextColor(255, 255, 255, 255)
+        surface.DrawText(text_date)
+
+        cam.End2D()
+
+        render.PopRenderTarget()
+
+        rtsurf:SetTexture("$basetexture", rtmat)
+
+        model:SetSubMaterial()
+
+        model:SetSubMaterial(1, "models/cod2019/shared/gunscreen")
+    end
+end
+
+ARC9.LoadAttachment(ATT, "cod2019_cosmetic_clock")
